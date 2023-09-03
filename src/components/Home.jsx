389ba9c4e12a2c729031b628e048f70027590635
rajan <rajanshahi1267@gmail.com>
2023-09-03 09:39:02 -0700

    complete home container

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,23 +3,29 @@ import { BsFillArrowRightCircleFill } from "react-icons/bs";
 
 const Home = () => {
   return (
-    <div className=" bg-[#0a192f] w-full h-screen">
-      {/* container */}
-      <div className=" max-w[1000px] mx-auto px-8 flex flex-col justify-center h-full">
-        <p className=" text-pink-600">Hi, My name is</p>
-        <h1 className=" text-4xl sm:text-7xl font-bold text-[#ccd6f6]">Rajan Shahi</h1>
-        <h2 className=" text-4xl sm:text-7xl font-bold text-[#8892b0]">I'm a frontend Developer.</h2>
-        <p className=" text-[#8892b0] py-4 max-w-[700px]">
-          I’m a Frontend developer specializing in building (and occasionally
+    <div name='home' className='w-full h-screen bg-[#0a192f]'>
+      {/* Container */}
+      <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+        <p className='text-pink-600'>Hi, my name is</p>
+        <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
+          Rajan Shahi
+        </h1>
+        <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>
+          I'm a  frontend Developer.
+        </h2>
+        <p className='text-[#8892b0] py-4 max-w-[700px]'>
+          I’m a frontend developer specializing in building (and occasionally
           designing) exceptional digital experiences. Currently, I’m focused on
           building responsive frontend web applications.
-          <div>
-            <button className=" text-white  border-2 px-6 py-3 my-2 flex items-center  hover:bg-pink-600 hover:border-pink-600">View Work 
-            <span className=" hover:rotate-90 duration-300"><BsFillArrowRightCircleFill className=" ml-3"/>
-            </span>
-            </button>
-          </div>
         </p>
+        <div>
+          <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
+            View Work
+            <span className='group-hover:rotate-90 duration-300'>
+              <BsFillArrowRightCircleFill className='ml-3 ' />
+            </span>
+          </button>
+        </div>
       </div>
     </div>
   );
