ee237a1711848ad9259dfa66dc61b082739247e9
rajan <rajanshahi1267@gmail.com>
2023-09-03 18:37:47 -0700

    add

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -46,7 +46,7 @@ const handleClick = () => setNev(!nav)
             </a>
           </li>
           <li className=" w-[160px] h-[60px] flex justify-between items-center ml-[-100px]  hover:ml-[-10px] duration-300 bg-[#333333]">
-            <a className=" flex justify-between items-center w-full text-gray-300" href="/">
+            <a className=" flex justify-between items-center w-full text-gray-300" href="https://github.com/rajan-shahi" target="_blank">
               Github <BsGithub size={30}/>
             </a>
           </li>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,14 +4,14 @@ import CSS from '../assets/css.png';
 import JavaScript from '../assets/javascript.png';
 import ReactImg from '../assets/react.png';
 import Node from '../assets/node.png';
-import FireBase from '../assets/firebase.png';
+
 import GitHub from '../assets/github.png';
 import Tailwind from '../assets/tailwind.png';
 import Mongo from '../assets/mongo.png';
 
 const Skills = () => {
   return (
-    <div name="skills" className=' w-full h-screenbg-[#0a192f] text-gray-300'>
+    <div name="skills" className=' w-full h-screen bg-[#0a192f] text-gray-300'>
         {/* container */}
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
             <div>
