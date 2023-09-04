3cfc279346d1c099de62c26345c8caeabf5942c6
rajan <rajanshahi1267@gmail.com>
2023-09-04 09:24:46 -0700

    add my Email link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -89,7 +89,7 @@ const Navbar = () => {
           <li className=" w-[160px] h-[60px] flex justify-between items-center ml-[-100px]  hover:ml-[-10px] duration-300 bg-blue-600">
             <a
               className=" flex justify-between items-center w-full text-gray-300"
-              href="/"
+              href="http://www.linkedin.com " target="_blank"
             >
               Linkedin <BsLinkedin size={30} />
             </a>
@@ -106,7 +106,7 @@ const Navbar = () => {
           <li className=" w-[160px] h-[60px] flex justify-between items-center ml-[-100px]  hover:ml-[-10px] duration-300 bg-[#6fc2b0]">
             <a
               className=" flex justify-between items-center w-full text-gray-300"
-              href="/"
+              href="https://rajanshahi1267@gmail.com"
             >
               Email <HiOutlineMail size={30} />
             </a>
