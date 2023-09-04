1e90e09736bdbc3bf404450382334f982dcadd55
rajan <rajanshahi1267@gmail.com>
2023-09-04 08:45:07 -0700

    add

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -57,11 +57,31 @@ const Navbar = () => {
             : "absolute top-0 left-0  w-full h-screen bg-[#0a192f] flex flex-col justify-center  items-center"
         }
       >
-        <li className=" py-4 text-4xl">Home</li>
-        <li className=" py-4 text-4xl">About</li>
-        <li className=" py-4 text-4xl">Skills</li>
-        <li className=" py-4 text-4xl">Works</li>
-        <li className=" py-4 text-4xl">Contacts</li>
+        <li className=" py-4 text-4xl">
+        <Link onClick={handleClick} to="home" spy={true} smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li className=" py-4 text-4xl">
+        <Link onClick={handleClick} to="about" spy={true} smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li className=" py-4 text-4xl">
+        <Link onClick={handleClick} to="skills" spy={true} smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li className=" py-4 text-4xl">
+        <Link onClick={handleClick} to='works' smooth={true} duration={500}>
+            Works
+          </Link>
+        </li>
+        <li className=" py-4 text-4xl">
+        <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
       </ul>
       {/* social icons */}
       <div className=" hidden lg:flex fixed flex-col top-[35%] left-0">
