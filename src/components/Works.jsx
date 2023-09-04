d943c28b81e20111c3f0f3b621eaf2ccde904ed6
rajan <rajanshahi1267@gmail.com>
2023-09-04 08:54:58 -0700

    add another react hook

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -4,7 +4,7 @@ import realState from "../assets/realestate.jpg";
 
 const Works = () => {
   return (
-    <div name="works" className=" w-full md:h-screen text-gray-300 bg-[#0a192f]">
+    <div className=" w-full md:h-screen text-gray-300 bg-[#0a192f]">
       <div className=" max-w-[1000px] mx-auto  p-4 flex flex-col justify-center w-full h-full">
         <div className=" pb-8">
           <p className=" text-4xl font-bold inline border-b-4  text-gray-300 border-pink-600">
