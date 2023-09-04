3a257a8cb73569fae71f6c22974b94dfed63ac4a
rajan <rajanshahi1267@gmail.com>
2023-09-03 18:54:57 -0700

    add

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from './components/About';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 import Skills from './components/Skills';
+import Works from './components/Works';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
     <Home/>
     <About/>
     <Skills/>
+    <Works/>
     </div>
   );
 }
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -15,7 +15,7 @@ const Skills = () => {
         {/* container */}
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
             <div>
-                <p className=' text-4xl font-bold   border-b-4 inline border-pink-600'>Experiences</p>
+                <p className=' text-4xl font-bold   border-b-4 inline border-pink-600'>Skills</p>
                 <p className=' py-4'>// These are the technologies I've worked with</p>
             </div>
             <div className=' w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
